6e6ca5b469125dd66807089425e1559bb08c6e9a
Maciej Łotysz <maciejlotysz@gmail.com>
2023-11-27 10:41:26 +0100

    task 2023-11-27

--- tasks/2023-11-27/index.ts
+++ tasks/2023-11-27/index.ts
@@ -1,1 +1,8 @@
-// Tutaj skopiuj kod zadania
+export function calculateTilesNeeded(sections: Array<{ width: number; height: number; }>): number {
+  // let totalTiles = 0;
+  // for (let i = 0; i < sections.length; i++) {
+  //   totalTiles += sections[i].width * sections[i].height;
+  // }
+  // return totalTiles;
+  return sections.reduce((acc, section) => acc + section.width * section.height, 0);
+}
