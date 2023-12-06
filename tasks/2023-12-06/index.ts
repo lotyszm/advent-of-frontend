6930a00d0bcd3ce8eb1045329e7beeffbead45bb
Maciej Łotysz <maciejlotysz@gmail.com>
2023-12-06 19:15:16 +0100

    task 2023-12-06

---
+++ tasks/2023-12-06/index.ts
@@ -0,0 +1,41 @@
+enum InvalidState {
+  unknown = "unknown",
+}
+
+export class OrderController {
+  machines = new Set<Machine>();
+
+  registerMachine(machine: Machine) {
+    this.machines.add(machine);
+  }
+
+  unregisterMachine(machine: Machine) {
+    this.machines.delete(machine);
+  }
+
+  setState(state: string) {
+    if (state === InvalidState.unknown) {
+      throw new Error("Invalid state provided");
+    }
+    this.machines.forEach((machine) => {
+      machine.state = state;
+      machine.addToLog(state);
+    });
+  }
+}
+
+export class Machine {
+  private statesLog: string[] = [];
+
+  state: string | null = null;
+
+  performAudit() {
+    return this.statesLog.map((state, index) => {
+      return `Order #${index + 1} - ${state}`;
+    });
+  }
+
+  addToLog(state: string) {
+    this.statesLog.push(state);
+  }
+}
